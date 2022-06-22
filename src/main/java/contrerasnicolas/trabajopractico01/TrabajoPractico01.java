69e8ff38f7d84a0ba009d58a75203b11081d87a2
nicolas <nicolas@sindragosa>
2022-06-21 22:01:32 -0300

    v0.1
    Inicializacion

---
+++ src/main/java/contrerasnicolas/trabajopractico01/TrabajoPractico01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package contrerasnicolas.trabajopractico01;
+
+/**
+ *
+ * @author Nicolas Contreras
+ */
+public class TrabajoPractico01 {
+
+    public static void main(String[] args) {
+       VentanaPrincipal a = new VentanaPrincipal();
+       a.setVisible(true);
+    }
+}
